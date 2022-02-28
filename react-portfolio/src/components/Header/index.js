a3c55cad67d1f06a310c718058995386592adc6a
ebtaterbug <ityjoseph@gmail.com>
2022-02-28 06:57:42 -0800

    add header and navigation

---
+++ react-portfolio/src/components/Header/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Route, Redirect, HashRouter } from "react-router-dom";
+import Navigation from "../../components/Navigation";
+import About from "../../components/About";
+import Portfolio from "../../components/Project";
+import Contact from '../../components/Contact';
+import Resume from '../../components/Resume';
+
+
+function Header() {
+    return (
+      <HashRouter>
+        <header>
+          <Navigation />
+        </header>
+
+        <div className="content">
+          <Route exact path="/" render={() => <Redirect to="/about" />} />
+          <Route path="/about" component={About} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact}/>
+          <Route path="/resume" component={Resume}/>
+        </div>
+      </HashRouter>
+    );
+}
+
+export default Header;
