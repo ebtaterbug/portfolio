6b064e39acea1da22f39e856324418fe34b644f2
ebtaterbug <ityjoseph@gmail.com>
2022-03-18 09:41:38 -0700

    add letschat to projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import CollaborativeCargo from './collaborative-cargo.PNG'
 import RunBuddy from './RunBuddy.PNG'
 import WeathertoCamp from './weathertocamp.PNG'
+import LetsChat from './letschat.PNG'
 
 function Portfolio() {
 
     return (
         <div className="container">
-                <div className="my-5 center">
+            <div className="my-5 center">
                     <img src={CollaborativeCargo} alt="CollaborativeCargo" />
                     <h3>Collaborative Cargo</h3>
                     <a href="https://github.com/ebtaterbug/collaborative-cargo" target="_blank">
@@ -16,17 +17,17 @@ function Portfolio() {
                     <a href="https://collaborative-cargo.herokuapp.com/" target="_blank">
                         <button>Live Site</button>
                     </a>
-                </div>
+            </div>
             <div className="my-5 center">
-                    <img src={RunBuddy} alt="RunBuddy" />
-                    <h3>Run Buddy</h3>
-                    <a href="https://github.com/ebtaterbug/run-buddy" target="_blank">
+                    <img src={LetsChat} alt="WeathertoCamp" />
+                    <h3>Weather To Camp</h3>
+                    <a href="https://github.com/ebtaterbug/lets-chat" target="_blank">
                         <button >GitHub</button>
                     </a>
-                    <a href="https://ebtaterbug.github.io/run-buddy/" target="_blank">
+                    <a href="https://vast-temple-57974.herokuapp.com/" target="_blank">
                         <button>Live Site</button>
                     </a>
-                </div>
+            </div>
             <div className="my-5 center">
                     <img src={WeathertoCamp} alt="WeathertoCamp" />
                     <h3>Weather To Camp</h3>
